6cfa1ff4c28d83ef087ef412a2cc89573ead7f85
Jésus Baro <118184872+JesusBaro@users.noreply.github.com>
2023-02-28 00:18:57 -0300

    aula 27/02

--- aulas-react/src/App.js
+++ aulas-react/src/App.js
@@ -1,3 +1,7 @@
+//--------------------------------------------------------------------
+//S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2
+//--------------------------------------------------------------------
+
 //aula set state e manipulacao de eventos (on click) 24/02
 // https://codesandbox.io/s/setstate-noite-ybir05?file=/src/App.js
 
@@ -120,3 +124,205 @@
 // }
 
 // }export default Exemplo2
+
+//REVISADO
+//------------------------------------------------------------
+//S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2-S2
+//------------------------------------------------------------
+
+//AULA 27/02/23
+//Ciclos de vida
+
+// import React, {Component} from 'react'
+
+// class App extends Component{
+//     state={
+//         cor:"red"
+//     }
+
+// mudarCor= ()=>{
+//     this.setState({
+//         cor:"blue"
+//     })
+// }
+
+
+// render(){
+//     return(
+// <>
+//  <h1>{this.state.cor}</h1>
+//  <button onClick={this.mudarCor}> mudar cor</button>
+// </>
+//     )
+// }
+
+// }export default App
+
+// --------- contador simples
+
+// import React, { Component } from 'react';
+
+// class Contador extends Component {
+
+//     state = {
+//         numero: 0
+//     }
+
+
+//     //contador ilimitado
+//     // adicionar =() =>{
+//     //     this.setState({
+//     //         numero: this.state.numero +1
+//     //     })
+//     // }
+
+//     // remover =() =>{
+//     //     this.setState({
+//     //         numero: this.state.numero -1
+//     //     })
+//     // }
+
+
+
+//     //contador limitado
+//     // adicionar = () => {
+//     //     if (this.state.numero < 10) {
+//     //         this.setState({
+//     //             numero: this.state.numero + 1
+//     //         })
+//     //     }
+//     // }
+
+
+//     // remover = () => {
+//     //     if (this.state.numero > 0) {
+//     //         this.setState({
+//     //             numero: this.state.numero - 1
+//     //         })
+//     //     }
+
+//     // }
+
+
+//         render(){
+//             return (
+//                 <>
+//                     <h1> {this.state.numero} </h1>
+//                     {/*  o numero exibido precisa ser chamado através do estate  */}
+//                     <button onClick={this.adicionar}>Adicionar</button>
+//                     <button onClick={this.remover}>Remover</button>
+//                 </>
+//             )
+//         }
+//     }
+// export default Contador;
+
+
+///exempo 2 
+
+import React, { Component } from 'react';
+
+class ContadorAutomatizado extends Component {
+
+    state = {
+        numero: 0,
+        msg: "oi",
+        nome:"Oi"
+    }
+
+//   contador limitado
+    adicionar = () => {
+        if (this.state.numero < 10) {
+            this.setState({
+                numero: this.state.numero + 1
+            })
+        }
+    }
+
+
+    remover = () => {
+        if (this.state.numero > 0) {
+            this.setState({
+                numero: this.state.numero - 1
+            })
+        }
+
+    }
+
+
+    //component didMount - pré renderiza algo depois de renderizar  index.html
+
+    // componentDidMount(){
+    //     document.title = this.state.msg
+    // }
+
+// component didUpdate -- atualiza quando algun dado é atualizado, ele observa se tem alguma mudança e executa uma funcao
+// componentDidUpdate(){
+//     document.title = this.state.numero
+
+// }
+
+
+// =====================
+
+// setInterval() - método que chama uma funçao em um intervalo de tempo
+// setInterval(() => {acao},{tempo})
+iniciar = () => {
+  const parar =  setInterval(()=>{
+        this.setState({
+        numero:this.state.numero +1
+    })},1000);
+    this.parar = () => {
+        clearInterval(parar)
+    }
+}
+
+//clearInterval () é um método que para o método setInterval
+//necessario criar uma variavel para interligar a funcao iniciar..
+// dentro da função o clearInterval
+// const parar =  setInterval(()=>{
+//     this.setState({
+//     numero:this.state.numero +1
+// })},1000);
+// this.parar = () => {
+//     clearInterval(parar)
+// // }
+
+resetar =() =>{
+    this.setState({
+        numero:0
+    })
+}
+
+
+//exemplo mudando a cor da body apos 2s e adiconando valor na prop nome
+//setTimeOut: executa funcao depois de um tempo determinado
+
+teste = () =>{
+    setTimeout(()=>{
+        document.body.style.backgroundColor = "orange"
+        this.setState({
+nome: "Jésus"
+        })
+    },2000)}
+
+
+        render(){
+            return (
+                <>
+                    <h1> {this.state.numero} </h1>
+                    {/*  o numero exibido precisa ser chamado através do estate  */}
+                    {/* <button onClick={this.adicionar}>Adicionar</button> */}
+                    {/* <button onClick={this.remover}>Remover</button> */}
+                    <button onClick={this.iniciar}>Iniciar</button>
+                    <button onClick={this.parar}>Parar</button>
+                    <button onClick={this.resetar}>Reset</button>
+
+                    <h2>{this.state.nome}</h2>
+                    <button onClick={this.teste}>Teste Body</button>
+
+                </>
+            )
+        }
+    }
+export default ContadorAutomatizado;
